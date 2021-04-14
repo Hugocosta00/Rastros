9ac657ee575ef642c2aad00e9dea7531a7140572
joa0neves <unfit4winter@gmail.com>
2021-04-14 14:57:49 +0100

    first commit

---
+++ src/rastros/Jogo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rastros;
+
+/**
+ *
+ * @author Joao
+ */
+public class Jogo {
+    char[][] board = new char[7][7];
+    int size = 7; 
+    int[] currSquare = {5,5};   //initial square is 5,5
+    
+    public Jogo() {
+    }
+    
+    
+    
+    
+    
+}
---
+++ src/rastros/Rastros.java
@@ -0,0 +1,17 @@
+
+package rastros;
+
+/**
+ *
+ * @author Joao
+ */
+public class Rastros {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
